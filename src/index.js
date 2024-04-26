160981d0962676a6920a6635fe4b48c93bd787ed
Shkaffez <58544194+Shkaffez@users.noreply.github.com>
2024-04-26 15:03:09 +0300

    add DB

--- src/index.js
+++ src/index.js
@@ -1,55 +1,23 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server-express');
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
-
-let notes = [
-  { id: '1', content: 'This is a note', author: 'Adam Scott' },
-  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
-];
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-    notes: [Note!]!
-    note(id: ID!): Note!
-  }
-
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find((note) => note.id === args.id);
-    },
-  },
-  Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
-        content: args.content,
-        author: 'Adam Scott',
-      };
-      notes.push(noteValue);
-      return noteValue;
-    },
-  },
-};
+const DB_HOST = process.env.DB_HOST;
 
 const app = express();
-const server = new ApolloServer({ typeDefs, resolvers });
+db.connect(DB_HOST);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  },
+});
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen({ port }, () =>
