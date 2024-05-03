d40adc30bb19e3e6da8094f1ec60201ad56e203e
Shkaffez <58544194+Shkaffez@users.noreply.github.com>
2024-05-03 15:29:17 +0300

    add signUp and auth with jwt

--- src/index.js
+++ src/index.js
@@ -5,17 +5,32 @@ const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 
 const app = express();
 db.connect(DB_HOST);
+
+const getUser = (token) => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      new Error('Session invalid');
+    }
+  }
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    return { models };
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    const user = getUser(token);
+    console.log(user);
+    return { models, user };
   },
 });
 server.applyMiddleware({ app, path: '/api' });
--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  scalar DateTime
   type Query {
     notes: [Note!]!
     note(id: ID!): Note!
@@ -10,9 +11,23 @@ module.exports = gql`
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String!
+    notes: [Note!]!
   }
 
   type Mutation {
-    newNote(content: String!, author: String): Note!
+    newNote(content: String!): Note!
+    updateNote(id: ID!, content: String!): Note!
+    deleteNote(id: ID!): Boolean!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
   }
 `;
